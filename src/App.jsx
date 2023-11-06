5b4455a3f333fae300115bfe2e1003efca5cbf4b
Harley Brackley <harleybrackley@gmail.com>
2023-11-06 16:21:44 +1100

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <header>1</header>
+      <>2</>
+      <>3</>
+      <>4</>
+      <footer>5</footer>
+    </>
+  );
+}
+
+export default App;
